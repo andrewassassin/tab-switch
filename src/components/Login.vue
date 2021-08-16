71daa961fe03ae74a3bce7f1973cfc05444888d3
andrew <r04631044@ntu.edu.tw>
2021-08-16 17:06:52 +0800

    turn form and loading file pop up  to vue version

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -4,7 +4,7 @@
     <section class="py-3">
         <div class="container">
             <div class="row">
-                <form id="LoginForm" class="container col-md-6 py-3">
+                <form id="LoginForm" v-on:submit.prevent="loginForm($event)" class="container col-md-6 py-3">
                     <div class="form-group">
                         <label for="userName">會員帳號</label>
                         <input type="text" placeholder="請輸入帳號" id="userName" class="form-control" required>
@@ -14,7 +14,7 @@
                         <input type="password" placeholder="請輸入密碼" id="userPwd" class="form-control"  required>
                     </div>
                     <div class="form-group my-4">
-                        <button type="submit" class="btn btn-primary"><i class="fas fa-plus"></i> 註冊</button>
+                        <button type="submit" class="btn btn-primary"><i class="fas fa-plus"></i> 登入</button>
                     </div>
                 </form>
             </div>
@@ -33,25 +33,24 @@ export default {
         msg: '會員登入'
         }
     }, 
-    mounted(){
-        $("#LoginForm").unbind('submit').bind('submit',function(e){
+    methods:{
+        loginForm (event) {
             console.log('submit')
-            e.preventDefault();
             const data = {
-            username: $("#userName").val(),
-            password:$("#userPwd").val()
+                username: event.target[0].value,
+                password: event.target[1].value
             }
             const config = {
-            responseType: 'text'
+              responseType: 'text'
             };
             axios.post("http://localhost/Amitproject/login.php#/", data, config)
                 .then(response => {
-                console.log('res  ', response);
+                    console.log('res  ', response);
                 })
                 .catch(error => {
-                console.log('err',error);
+                    console.log('err',error);
                 });
-        })     
+        }
     }
 }
 </script>
